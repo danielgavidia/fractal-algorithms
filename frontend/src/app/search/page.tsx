9b68b99ff1f650e812879a547a7e0d6af000e34c
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-15 15:55:36 -0400

    Added initial visualization for linear search

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,14 +1,56 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+type Node = {
+	list: number[];
+	target: number;
+	index: number;
+	success: boolean;
+};
+
+interface AnimationProps {
+	node: Node;
+}
+
+const Animation = ({ node }: AnimationProps) => {
+	const { list, target, index, success } = node;
+	// const border =
+	const getBorder = (item: number, key: number, index: number, target: number): string => {
+		if (key === index) {
+			return "border-2 border-red-900";
+		} else if (item === target) {
+			return "border-2 border-green-900";
+		} else {
+			return "";
+		}
+	};
+
+	const generalBackground = success ? "bg-white" : "";
+	return (
+		<div>
+			<ul className={"flex justify-between " + generalBackground}>
+				{list.map((i, key) => {
+					const border = getBorder(i, key, index, target);
+					console.log("item", i);
+					console.log("target", target);
+					return (
+						<li key={key} className={border}>
+							{i}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
 
 const page = () => {
 	const [list, setList] = useState<number[]>([54, 4, 27, 36, 22]);
 	const [newNumber, setNewNumber] = useState<number>(0);
 	const [target, setTarget] = useState<number>(0);
-	const [algoOutput, setAlgoOutput] = useState<string>("");
+	const [algoOutput, setAlgoOutput] = useState<Node[] | null>(null);
 
 	const handleSetTarget = (i: number) => {
 		setTarget(i);
@@ -35,19 +77,28 @@ const page = () => {
 	useEffect(() => {
 		const fetch = async () => {
 			const res = await getAlgoOutput();
-			setAlgoOutput(JSON.stringify(res));
+			console.log(res);
+			setAlgoOutput(res);
 		};
 		fetch();
 	}, [target]);
+
+	const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+	useEffect(() => {
+		if (algoOutput !== null && currentIndex < algoOutput.length - 1) {
+			const timer = setTimeout(() => {
+				setCurrentIndex((prevIndex) => prevIndex + 1);
+			}, 2000);
+
+			return () => clearTimeout(timer);
+		}
+	}, [currentIndex, algoOutput]);
 
 	console.log("target", target);
 
 	return (
 		<div className="p-4 w-full">
-			{/* <form className="w-full flex">
-				<input value={list} />
-				<input value={target} />
-			</form> */}
 			<h2>Linear Search Algo</h2>
 			<form onSubmit={handleAddToList} className="w-full">
 				<input
@@ -66,7 +117,18 @@ const page = () => {
 					</li>
 				))}
 			</ul>
-			<div className="text-green-400">{algoOutput}</div>
+			<div className="text-green-400">
+				{algoOutput && (
+					<div>
+						{algoOutput.slice(0, currentIndex + 1).map((x, key) => {
+							return <Animation key={key} node={x} />;
+						})}
+					</div>
+					// <div>
+					// 	<Animation node={algoOutput[currentIndex]} />
+					// </div>
+				)}
+			</div>
 		</div>
 	);
 };
