2c957aaf7d0cf745a368aa21022feeffcc09e357
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-16 15:59:12 -0400

    Improved animation for merge sort

--- frontend/src/components/sort/Merge.tsx
+++ frontend/src/components/sort/Merge.tsx
@@ -8,46 +8,67 @@ interface MergeProps {
 const Merge = ({ node }: MergeProps) => {
 	const { list, level, left, right } = node;
 	// const generalBackground = success ? "bg-white" : "";
+	// const width = Math.floor(1000 / list.length);
 
 	return (
-		<div className={"flex w-full border-2 border-white"}>
+		<div className={"flex flex-col w-full border-2 border-white"}>
 			<div>
 				<p>Level: {level}</p>
 			</div>
 			<p>List</p>
 
-			<ul className="flex justify-between w-full">
+			<ul className="flex justify-between w-full items-end min-h-[250px]">
 				{list.map((i, key) => {
 					return (
-						<li key={key} className="border-2 border-white">
-							{i}
+						<li
+							key={key}
+							// style={{ width: `${width}px` }}
+							className="flex-1 mx-1 border-2 border-white"
+						>
+							<div style={{ height: `${i * 2}px` }}></div>
+							<div className="text-center">{i}</div>
 						</li>
 					);
 				})}
 			</ul>
-			<p>Left</p>
-
-			<ul className="flex justify-between w-full">
-				{left &&
-					left.map((i, key) => {
-						return (
-							<li key={key} className="border-2 border-green">
-								{i}
-							</li>
-						);
-					})}
-			</ul>
-			<p>Right</p>
-			<ul className="flex justify-between w-full">
-				{right &&
-					right.map((i, key) => {
-						return (
-							<li key={key} className="border-2 border-red">
-								{i}
-							</li>
-						);
-					})}
-			</ul>
+			<div className="flex w-full">
+				<div className="w-full">
+					<p>Left</p>
+					<ul className="flex justify-between w-full items-end min-h-[250px]">
+						{left &&
+							left.map((i, key) => {
+								return (
+									<li
+										key={key}
+										// style={{ width: `${width}px` }}
+										className="flex-1 mx-1 border-2 border-white"
+									>
+										<div style={{ height: `${i * 2}px` }}></div>
+										<div className="text-center">{i}</div>
+									</li>
+								);
+							})}
+					</ul>
+				</div>
+				<div className="w-full">
+					<p>Right</p>
+					<ul className="flex justify-between w-full items-end min-h-[250px]">
+						{right &&
+							right.map((i, key) => {
+								return (
+									<li
+										key={key}
+										// style={{ width: `${width}px` }}
+										className="flex-1 mx-1 border-2 border-white"
+									>
+										<div style={{ height: `${i * 2}px` }}></div>
+										<div className="text-center">{i}</div>
+									</li>
+								);
+							})}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
