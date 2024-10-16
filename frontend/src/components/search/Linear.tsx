3824809a974792c8ac429d345178bb89589a15e6
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-16 12:00:26 -0400

    Added bars instead of numbers and color codes

--- frontend/src/components/search/Linear.tsx
+++ frontend/src/components/search/Linear.tsx
@@ -22,26 +22,32 @@ interface LinearAnimationProps {
 }
 
 const LinearAnimation = ({ node }: LinearAnimationProps) => {
-	const { list, target, index, success } = node;
-	const getBorder = (item: number, key: number, index: number, target: number): string => {
-		if (key === index) {
+	const { list, target, index } = node;
+	const getBarStyle = (item: number, key: number, index: number, target: number): string => {
+		if (key === index && item === target) {
+			return "border-2 border-green-900 bg-white";
+		} else if (key === index) {
 			return "border-2 border-red-900";
 		} else if (item === target) {
 			return "border-2 border-green-900";
 		} else {
-			return "";
+			return "border-2 border-white";
 		}
 	};
-	const generalBackground = success ? "bg-white" : "";
+	// const generalBackground = success ? "bg-white" : "";
 
 	return (
 		<div>
-			<ul className={"flex justify-between " + generalBackground}>
+			<ul className={"flex justify-between items-end"}>
 				{list.map((i, key) => {
-					const border = getBorder(i, key, index, target);
+					const barStyle = getBarStyle(i, key, index, target);
 					return (
-						<li key={key} className={border}>
-							{i}
+						<li
+							key={key}
+							className={`flex-1 mx-1 ${barStyle}`}
+							style={{ height: `${i * 5}px` }}
+						>
+							<span className="block text-center">{i}</span>
 						</li>
 					);
 				})}
