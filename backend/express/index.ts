403e6be5e437d138b2b117f4ae334f32acbaf836
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-17 14:34:40 -0400

    Eliminated CORS temporarily

--- backend/express/index.ts
+++ backend/express/index.ts
@@ -5,11 +5,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 const cors = require("cors");
 
-app.use(
-	cors({
-		origin: process.env.FRONTEND_URL,
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: process.env.FRONTEND_URL,
+// 	})
+// );
+
+app.use(cors());
 
 console.log(process.env.FRONTEND_URL);
 
